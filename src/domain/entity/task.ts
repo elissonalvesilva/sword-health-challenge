19f821acd5c0d17b3c3bc00f56d162e4e3424fe4
elissonsilva-mb <elisson.silva@mercadobitcoin.com.br>
2022-08-23 15:22:24 -0300

    feat: created task entity

---
+++ src/domain/entity/task.ts
@@ -0,0 +1,18 @@
+import { Task } from "domain/protocols";
+
+export const MAX_LENGTH_RESUME = 2500;
+export class TaskEntity {
+  constructor(){}
+
+  create(resume: string): [Task, boolean] {
+
+    if(resume.length > 2500) {
+      return [null as any, false];
+    }
+
+    return [{
+      resume,
+      createdAt: new Date(),
+    }, true];
+  }
+}
