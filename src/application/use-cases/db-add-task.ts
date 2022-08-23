dceea6929e1134caaa50f252140df26bc5268a2f
elissonsilva-mb <elisson.silva@mercadobitcoin.com.br>
2022-08-22 22:13:22 -0300

    feat: integrate data with add repository

--- src/application/use-cases/db-add-task.ts
+++ src/application/use-cases/db-add-task.ts
@@ -4,9 +4,10 @@ import { AddTask } from "domain/use-cases";
 
 export class DbAddTask implements AddTask {
   constructor(private readonly addTaskRepository: AddTaskRepository){}
-  add(task: Task): Promise<boolean> {
 
-    const addedTask = this.addTaskRepository.addTask(task);
+  async add(task: Task): Promise<boolean> {
+
+    const addedTask = await this.addTaskRepository.addTask(task);
 
     return Promise.resolve(true);
   }
