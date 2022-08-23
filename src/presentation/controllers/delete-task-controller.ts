a0d5439970fca98e503f4dc9a852da3f02e33bf7
elissonsilva-mb <elisson.silva@mercadobitcoin.com.br>
2022-08-23 16:18:33 -0300

    feat: created delete controller

---
+++ src/presentation/controllers/delete-task-controller.ts
@@ -0,0 +1,33 @@
+import { DeleteTask } from "domain/use-cases/delete-task";
+import { MissingParamError, NotFoundParamError } from "presentation/errors";
+import { badRequest, ok, serverError } from "presentation/helpers";
+import { HttpResponse } from "presentation/protocols";
+import { Controller } from "presentation/protocols";
+
+export class DeleteTaskController implements Controller {
+
+  constructor(private readonly deleteTask: DeleteTask){}
+
+  async handle(request: any): Promise<HttpResponse> {
+    try {
+      const requiredFields = ['id'];
+
+      for (const field of requiredFields) {
+        if (!request[field]) {
+          return badRequest(new MissingParamError(field));
+        }
+      }
+
+      const deleted = await this.deleteTask.delete(request.id);
+
+      if(!deleted) {
+        return badRequest(new NotFoundParamError(request.id));
+      }
+
+      return ok({'message': 'deleted'})
+    } catch (error) {
+      return serverError(error as Error);
+    }
+  }
+
+}
--- src/presentation/controllers/index.ts
+++ src/presentation/controllers/index.ts
@@ -2,3 +2,4 @@ export * from './create-task-controller';
 export * from './update-task-controller';
 export * from './get-all-tasks-controller';
 export * from './get-task-controller';
+export * from './delete-task-controller';
