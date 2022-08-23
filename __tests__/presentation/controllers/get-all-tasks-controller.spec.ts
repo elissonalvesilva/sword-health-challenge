cf39f578f85a4d3447fe541b34f83ae82fd6139c
elissonsilva-mb <elisson.silva@mercadobitcoin.com.br>
2022-08-23 16:11:47 -0300

    test: created get and get all tasks controller

---
+++ __tests__/presentation/controllers/get-all-tasks-controller.spec.ts
@@ -0,0 +1,58 @@
+import { Task } from "domain/protocols";
+import { GetTask } from "domain/use-cases";
+import { GetAllTasksController } from "presentation/controllers";
+import { ok, serverError } from "presentation/helpers";
+import { fake } from "utils";
+
+const fakeTask = fake.fakeTask();
+const makeGetTask = (): GetTask => {
+  class GetTaskStub implements GetTask {
+    getAll(): Promise<Task[]> {
+      return Promise.resolve([fakeTask])
+    }
+    get(id: string): Promise<Task> {
+      return Promise.resolve(fakeTask)
+    }
+  }
+  return new GetTaskStub();
+}
+
+interface ControllerStub {
+  sut: GetAllTasksController;
+  getTask: GetTask
+}
+
+const makeSut = (): ControllerStub => {
+  const getTask = makeGetTask();
+  const sut = new GetAllTasksController(getTask);
+  return {
+    sut,
+    getTask,
+  };
+};
+
+describe('Get All Task', ()=> {
+  describe('Get All Task Controller', () => {
+    it('should return error when getAll throws', async () => {
+      const { sut, getTask } = makeSut();
+
+      jest.spyOn(getTask, 'getAll').mockRejectedValueOnce(new Error('happen some error'));
+
+      const httpRequest = {};
+
+      const httpResponse = await sut.handle(httpRequest);
+      expect(httpResponse).toEqual(serverError(new Error('happen some error')));
+    });
+
+    it('should return success on Get all tasks', async () => {
+      const { sut, getTask } = makeSut();
+
+      jest.spyOn(getTask, 'getAll').mockResolvedValueOnce([fakeTask]);
+
+      const httpRequest = {};
+
+      const httpResponse = await sut.handle(httpRequest);
+      expect(httpResponse).toEqual(ok([fakeTask]));
+    });
+  });
+});
