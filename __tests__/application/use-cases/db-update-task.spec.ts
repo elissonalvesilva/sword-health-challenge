7223084aa1d0254fcf4301e3f16f217405ecb33c
elissonsilva-mb <elisson.silva@mercadobitcoin.com.br>
2022-08-23 13:49:07 -0300

    test: created update task on application layer

---
+++ __tests__/application/use-cases/db-update-task.spec.ts
@@ -0,0 +1,61 @@
+import { UpdateTaskRepository } from "application/protocols";
+import { Task } from "domain/protocols";
+import { DbUpdateTask } from "application/use-cases";
+import { fake } from "utils";
+
+interface SutTypes {
+  sut: DbUpdateTask;
+  updateTaskRepository: UpdateTaskRepository
+}
+
+const fakeTask = fake.fakeTask();
+const makeUpdateTaskRepository = (): UpdateTaskRepository => {
+  class UpdateTaskRepositoryStub implements UpdateTaskRepository {
+    updateTask(id: string, task: Task): Promise<boolean> {
+      return Promise.resolve(true)
+    }
+
+  }
+  return new UpdateTaskRepositoryStub();
+}
+
+
+const makeSut = (): SutTypes => {
+  const updateTaskRepository = makeUpdateTaskRepository();
+  const sut = new DbUpdateTask(updateTaskRepository);
+
+  return {
+    sut,
+    updateTaskRepository,
+  }
+};
+
+describe('DbUpdateTask UseCase', () => {
+  describe('UpdateTaskRepository updateTask', () => {
+    it('should call updateTask with correct values', async () => {
+      const { sut, updateTaskRepository } = makeSut();
+
+      const updateTaskRepositorySpy = jest.spyOn(
+        updateTaskRepository,
+        'updateTask',
+      );
+      const fakeId = '123-fake-id';
+      await sut.update(fakeId, fakeTask);
+
+      expect(updateTaskRepositorySpy).toBeCalledWith(fakeId, fakeTask);
+    });
+
+    it('should call updateTask throws', async () => {
+      const { sut, updateTaskRepository } = makeSut();
+
+      jest
+      .spyOn(updateTaskRepository, 'updateTask')
+      .mockReturnValueOnce(
+        new Promise((resolve, reject) => reject(new Error()))
+      );
+
+      const promise = sut.update('123-fake-id', fakeTask)
+      await expect(promise).rejects.toThrow();
+    });
+  });
+});
