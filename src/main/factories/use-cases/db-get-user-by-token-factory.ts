abc1c518808207a5489ba75f19082e7c2d827964
elissonsilva-mb <elisson.silva@mercadobitcoin.com.br>
2022-08-25 18:14:20 -0300

    fix: fix path json

--- src/main/factories/use-cases/db-get-user-by-token-factory.ts
+++ src/main/factories/use-cases/db-get-user-by-token-factory.ts
@@ -1,11 +1,12 @@
 import fs from 'fs';
+import path from 'path';
 
 import { DbGetUserByToken } from '@/application/use-cases';
 import { GetUserByToken } from '@/domain/use-cases';
 import { AuthByJson } from '@/infra/auth';
 
 export const makeGetUserByToken = (): GetUserByToken => {
-  const json: string = fs.readFileSync(__dirname + 'user.json', 'utf8');
+  const json: string = fs.readFileSync(process.cwd() + '/users.json', 'utf8');
   const getUserByTokenImplementation = new AuthByJson(json);
   return new DbGetUserByToken(getUserByTokenImplementation);
 };
